20875e3a7382925507f90400e8de535e0b09f853
Viswanath Bodasakurthi <viswa.visu57@gmail.com>
2025-03-13 21:48:25 +0530

    updates pushed

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
-// config.js - Template file in your repository
-// This file will be processed during GitHub Actions deployment
-
-if (typeof window !== "undefined") {
-  window.firebaseConfig = {
-      apiKey: "__FIREBASE_API_KEY__",
-      authDomain: "__FIREBASE_AUTH_DOMAIN__",
-      databaseURL: "__FIREBASE_DATABASE_URL__",
-      projectId: "__FIREBASE_PROJECT_ID__",
-      storageBucket: "__FIREBASE_STORAGE_BUCKET__",
-      messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
-      appId: "__FIREBASE_APP_ID__"
-  };
-  
-  console.log("Firebase configuration loaded");
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyD2yDdpK2ZV_npTAIf9hslHcUAbNzWa80Q",
+  authDomain: "nazurul-sajida-wc.firebaseapp.com",
+  databaseURL: "https://nazurul-sajida-wc-default-rtdb.firebaseio.com",
+  projectId: "nazurul-sajida-wc",
+  storageBucket: "nazurul-sajida-wc.firebasestorage.app",
+  messagingSenderId: "669024462281",
+  appId: "1:669024462281:web:c24938ec420bd4c9167418",
+  measurementId: "G-03FBY54Y0L"
+};
